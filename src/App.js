f573187f89283207bb2dcf00a23f569ad17ba189
Yogesh Deshmukh <sampoorna.mahanti@tcs.com>
2021-05-23 08:15:01 +0530

    Added a new counter to frontend

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import FirstComponent from './components/learning-exercise/FirstComponent';
 import SecondComponent, {AdditionalComponent} from './components/learning-exercise/SecondComponent';
 import ThirdComponent from './components/learning-exercise/ThirdComponent';
 import './App.css';
+import Counter from './components/counter/Counter';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         My First React App - MyToDo Application
-        <FirstComponent></FirstComponent>
-        <SecondComponent></SecondComponent>
-        <ThirdComponent></ThirdComponent>
-        <AdditionalComponent></AdditionalComponent>
+        <Counter></Counter>
       </div>
     );
   }
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './Counter.css';
+
+class Counter extends Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            counter : 0
+        }
+
+        this.increment = this.increment.bind(this);
+    }
+
+    render(){
+        return(
+            <div className="Counter">
+            <button onClick = {this.increment}>+1</button>
+            <span className = "count">{this.state.counter}</span>    
+            </div>   
+        );
+    }
+
+    increment(){
+        console.log('increment')
+
+        this.setState({
+            counter : this.state.counter + 1
+        });
+    }
+
+}
+
+export default Counter;
